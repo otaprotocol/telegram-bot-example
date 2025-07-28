41044be1904a5e1bdda62b92bfff30568b413d11
Efe <efe@efebehar.dev>
2025-07-28 12:50:34 +0300

    add debug

--- src/run.ts
+++ src/run.ts
@@ -1,6 +1,9 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { Context, Telegraf } from 'telegraf';
+import createDebug from 'debug';
 import { Update } from 'telegraf/types';
+
+const debug = createDebug('bot:dev');
 
 const PORT = (process.env.PORT && parseInt(process.env.PORT, 10)) || 3000;
 const VERCEL_URL = `${process.env.VERCEL_URL}`;
@@ -10,8 +13,8 @@ const run = async (
     res: VercelResponse,
     bot: Telegraf<Context<Update>>,
 ) => {
-    console.log('Bot runs in production mode');
-    console.log(`setting webhook: ${VERCEL_URL}`);
+    debug('Bot runs in production mode');
+    debug(`setting webhook: ${VERCEL_URL}`);
 
     if (!VERCEL_URL) {
         throw new Error('VERCEL_URL is not set.');
@@ -19,9 +22,9 @@ const run = async (
 
     const getWebhookInfo = await bot.telegram.getWebhookInfo();
     if (getWebhookInfo.url !== VERCEL_URL + '/api') {
-        console.log(`deleting webhook ${VERCEL_URL}`);
+        debug(`deleting webhook ${VERCEL_URL}`);
         await bot.telegram.deleteWebhook();
-        console.log(`setting webhook: ${VERCEL_URL}/api`);
+        debug(`setting webhook: ${VERCEL_URL}/api`);
         await bot.telegram.setWebhook(`${VERCEL_URL}/api`);
     }
 
@@ -30,6 +33,6 @@ const run = async (
     } else {
         res.status(200).json('Listening to bot events...');
     }
-    console.log(`starting webhook on port: ${PORT}`);
+    debug(`starting webhook on port: ${PORT}`);
 };
 export { run };
